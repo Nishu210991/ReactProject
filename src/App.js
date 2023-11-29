354121ec5dcdc7f65d897fcf1b8761aedd16d265
Nishu Chauhan <nishuchauhan@Nishus-MacBook-Air.local>
2023-11-29 20:38:41 +0530

    add 1 tutorial

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import { useState } from 'react';
 // import About from './components/About';
 
 function App() {
+  const[mode, setMode] = useState('dark');
+
+  const toggleMode = ()=>{
+    if (mode === 'light'){
+      setMode ('dark');
+  }
+   else{
+  setMode('light');
+   }
+}
+
   return (
     <>
 
-   <Navbar title="Tokyo" about="About Us"/>
+   <Navbar title="Tokyo" about="About Us" mode={mode} toggleMode={toggleMode}/>
    <div className="container">
     <TextForm heading="Write Your Comment" />
     {/* <About/> */}
