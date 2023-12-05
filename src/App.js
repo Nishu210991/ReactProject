28a2d9d58dc89be27780bc3b2552d3f3ba2689e9
Nishu Chauhan <nishuchauhan@Nishus-MacBook-Air.local>
2023-12-05 19:39:14 +0530

    Router

--- src/App.js
+++ src/App.js
@@ -1,36 +1,69 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import { useState } from 'react';
-// import About from './components/About';
+import Alert from './components/Alert';
+import About from './components/About';
+
+
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
-  const[mode, setMode] = useState('dark');
+  const[mode, setMode] = useState('light');
+  const[alert, setAlert] = useState(null)
+
+  const showAlert=(message, type)=>{
+    setAlert({
+          msg: message,
+          type: type
+        })
+    setTimeout(()=>{
+      setAlert(null);
+    },1500);
+  }
 
   const toggleMode = ()=>{
     if (mode === 'light'){
       setMode ('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "Textulits-DarkMode"
   }
    else{
-  setMode('light');
-   }
+        setMode('light');
+        document.body.style.backgroundColor = 'white';
+        showAlert("Light mode has been enabled", "success");
+        document.title = "Textulits-LightMode"
+
+       }
 }
 
   return (
     <>
+  <Router>
 
-   <Navbar title="Tokyo" about="About Us" mode={mode} toggleMode={toggleMode}/>
+   <Navbar title="Tokyo"  mode={mode} toggleMode={toggleMode} />
+   <Alert  alert={alert} />
    <div className="container">
-    <TextForm heading="Write Your Comment" />
-    {/* <About/> */}
+   <Routes>
+    <Route exact path='/about' element={<About/>} />
+    <Route exact path='/' element={<TextForm showAlert={showAlert} heading="Write Your Comment" mode={mode} />} />
+    </Routes>
    </div>
-   
-
+   </Router>
   
    </>
   );
   
+
 }
+
+// element={<TextForm showAlert={showAlert} heading="Write Your Comment" mode={mode} />}
 
 export default App;
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,21 +5,26 @@ export default function TextForm(props) {
         console.log("UpperCase was Clicked" + text);
         let newText = text.toUpperCase();
         setText(newText)
+        props.showAlert("Contverted into Upper Case", "success")
+
     }
 
     const handletoClick = ()=>{
         console.log("UpperCase was Clicked" + text);
         let newText = text.toLowerCase();
         setText(newText)
+        props.showAlert("Contverted into Lower Case", "success")
     }
 
     const handleOnChange = (event)=>{
         console.log("UpperCase Clicked");
         setText(event.target.value)
+      
     }
 
     const handleCopy = () => {
         navigator.clipboard.writeText(text);
+        props.showAlert("Text Copied", "success")
       }
 
     const handleClearText = () => {
@@ -30,17 +35,19 @@ export default function TextForm(props) {
     const handleRemoveSpace = () => {
         let newText = text.split(/[ ]+ /);
         setText(newText.join(" "))
+        props.showAlert("Removed Extra Space", "success")
     }
       
 
     const[text, setText] = useState("")
   return (
     <>
-    <div className='container'>   
+    <div className='container' style={{color: props.mode==='dark'?'white':'#042743' }}>  
+    <h1>{props.heading}</h1>
     <div className="mb-3">
     
-    <h1>{props.heading}</h1>
-    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+   
+    <textarea className="form-control" value={text} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743' }} onChange={handleOnChange} id="myBox" rows="8"></textarea>
     </div>
     <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
     <button className="btn btn-primary mx-1" onClick={handletoClick}>Convert to LowerCase</button>
@@ -50,12 +57,12 @@ export default function TextForm(props) {
 
 
     </div>
-    <div className="container mx-1">
+    <div className="container mx-1" style={{color: props.mode==='dark'?'white':'#042743' }}>
         <h1>Your Text Summary</h1>
         <p>{text.split(" ").length} words  {text.length} character</p>
         <p>{0.008 * text.split(" ").length}</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:'Enter Your Text Please'}</p>
 
     </div>
     </>
